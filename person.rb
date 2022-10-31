118ac48ef9f741562c09dea3ecc70d9ab9705b6e
Raga <ragawilliam570@gmail.com>
2022-10-31 14:52:56 +0300

    Add class Person with all methods & variables

---
+++ person.rb
@@ -0,0 +1,63 @@
+class Person
+  # Constructor with name, age, and parent_permission as parameter.
+  def initialize(age, name="unknown", parent_permission=true)
+    # Instance vars: @id, @name, and @age.
+    @id
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  # @id, @name and @age getter methods
+  def id
+    @id
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+
+  # Setter methods for @name, and @parent_permission
+  def name!(name)
+    @name = name
+  end
+
+  def age!(age)
+    @age = age
+  end
+
+  # Private method is_of_age?
+  def is_of_age?
+    if @age >= 18
+      return true
+    else
+      return false
+    end
+  end
+
+  # public method can_use_services?
+  def can_use_services?
+    if is_of_age? || @parent_permission
+      return true
+    end
+    return false
+  end
+
+  # Make method is_of_age? private
+  private :is_of_age?
+end
+
+p = Person.new(20, "William", false)
+puts p.age
+puts p.name
+puts p.can_use_services?
+p.age!(10)
+p.name!("John")
+puts p.age
+puts p.name
+puts p.can_use_services?
+
