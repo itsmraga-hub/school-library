dd44c71d0c64ff411a68caec8316d9ed27c4734e
Raga <ragawilliam570@gmail.com>
2022-10-31 16:51:55 +0300

    Add random number for @id

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   # Constructor with name, age, and parent_permission as parameter.
   def initialize(age, name = 'unknown', parent_permission: true)
     # Instance vars: @id, @name, and @age.
-    @id = nil
+    @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
