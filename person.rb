479fafa5202b0034253d39c22d25eeb31d0077df
Raga <ragawilliam570@gmail.com>
2022-10-31 15:43:27 +0300

    Fix final linter issues

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   # Constructor with name, age, and parent_permission as parameter.
   def initialize(age, name = 'unknown', parent_permission: true)
     # Instance vars: @id, @name, and @age.
-    @id
+    @id = nil
     @age = age
     @name = name
     @parent_permission = parent_permission
@@ -23,17 +23,17 @@ class Person
   end
 
   # Private method is_of_age?
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 
   # public method can_use_services?
   def can_use_services?
-    return true if is_of_age? || @parent_permission
+    return true if of_age? || @parent_permission
 
     false
   end
 
   # Make method is_of_age? private
-  private :is_of_age?
+  private :of_age?
 end
