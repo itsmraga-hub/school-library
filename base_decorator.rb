a8220215a89cae5eaad1a041c3cd56d315ec50a4
Raga <ragawilliam570@gmail.com>
2022-11-02 12:03:42 +0300

    Add base decorator class

---
+++ base_decorator.rb
@@ -0,0 +1,26 @@
+require_relative "nameable"
+
+class BaseDecorator < Nameable
+  def initialize(obj)
+    @nameable = obj
+  end
+
+  def correct_name
+    @nameable.name
+  end
+end
+
+# class CapitalizeDecorator < BaseDecorator
+#   def correct_name
+#     @nameable.name.capitalize
+#   end
+# end
+
+# class TrimmerDecorator < BaseDecorator
+#   def correct_name
+#     if @nameable.correct_name.length > 10
+#       return @nameable.correct_name.slice(0, 9)
+#     end
+#     @nameable.correct_name
+#   end
+# end
