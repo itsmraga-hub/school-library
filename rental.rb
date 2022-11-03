ff8fe650e455a90ed67568b7ef0757bdb5369e18
Raga <ragawilliam570@gmail.com>
2022-11-03 08:44:03 +0300

    Add belongs to side of rental

--- rental.rb
+++ rental.rb
@@ -9,13 +9,13 @@ class Rental
     person.rentals << self
   end
 
-  # def book=(book)
-  #   @book = book
-  #   book.rentals.push(self) unless book.rentals.include?(self)
-  # end
+  def book=(book)
+    @book = book
+    book.rentals.push(self) unless book.rentals.include?(self)
+  end
 
-  # def person=(person)
-  #   @person = person
-  #   person.rentals.push(self) unless person.rentals.include?(self)
-  # end
+  def person=(person)
+    @person = person
+    person.rentals.push(self) unless person.rentals.include?(self)
+  end
 end
