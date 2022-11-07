e3f685b2c2e72dd4371b03055b3f6a9fd25ed714
Raga <ragawilliam570@gmail.com>
2022-11-07 13:19:00 +0300

    Ensure main only starts the program

--- main.rb
+++ main.rb
@@ -1,54 +1,10 @@
-require_relative './app'
+require './operations'
+require './app'
 
-@app = App.new
-
-def list_options
-  puts '
-  Please choose one of the options: (1 - 7)
-    1. - List all books
-    2. - List all people
-    3. - Create a person
-    4. - Create a book
-    5. - Create a rental
-    6. - List all rentals for a given person id
-    7. Exit
-  '
-end
-
-def start_program
-  loop do
-    list_options
-    choice = gets.chomp.to_i
-    if choice.between?(1, 7)
-      if choice == 7
-        choose_operation(choice)
-      else
-        puts 'Thank you for using this app!'
-        exit
-      end
-    end
-  end
-end
-
-def choose_operation(opt)
-  case opt
-  when 1
-    @app.list_books
-  when 2
-    @app.list_peoples
-  when 3
-    @app.create_person
-  when 4
-    @app.create_book
-  when 5
-    @app.create_rental
-  when 6
-    @app.list_rentals
-  end
-end
+@operations = Operations.new
 
 def main
-  start_program
+  @operations.start_program
 end
 
 main
